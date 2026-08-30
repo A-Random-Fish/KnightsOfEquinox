53c85f42dd09569358ecd8853a7e4940c9c05a79
TheSnailL0rd <jacob.catninja@gmail.com>
2026-08-30 03:07:04 +0100

    birb move squared

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
+    Animator anim;
+
+    [Header("Animation Variables")]
+    [SerializeField] GameObject playerDisplay;
+    [SerializeField] float playerRotSmoothing;
+    Vector3 playerLookDirection;
     
     [Header("Movement")]
     [SerializeField] Transform orientation;
     Vector2 movementInput;
-    Vector3 movementDirection;
     Vector2 dodgeInput = new Vector2(0f,1f);
 
     [Header("Movement Variable")]
@@ -26,15 +31,17 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         dodgeCooldown -= Time.deltaTime;
 
-        if (movementInput != Vector2.zero)
+        if (movementInput != Vector2.zero && !isDodging)
         {
             dodgeInput = movementInput;
+            playerLookDirection = new Vector3(movementInput.x, 0f, movementInput.y);
         }
 
         if (!isDodging)
@@ -45,6 +52,13 @@ public class PlayerController : MonoBehaviour
         {
             rb.linearVelocity = new Vector3(dodgeInput.x * dodgeSpeed, rb.linearVelocity.y, dodgeInput.y * dodgeSpeed);
         }
+
+        
+        Quaternion targetRot = Quaternion.LookRotation(playerLookDirection);
+        playerDisplay.transform.rotation = Quaternion.Lerp(playerDisplay.transform.rotation, targetRot, playerRotSmoothing * Time.deltaTime);
+
+        //Animator Conditions
+        anim.SetBool("Moving", movementInput != Vector2.zero);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -56,6 +70,7 @@ public class PlayerController : MonoBehaviour
     {
         if (context.performed && dodgeCooldown <= 0f)
         {
+            anim.SetTrigger("Dodge");
             StartCoroutine("DodgeDuration");
             dodgeCooldown = dodgeCooldownLength;
         }
