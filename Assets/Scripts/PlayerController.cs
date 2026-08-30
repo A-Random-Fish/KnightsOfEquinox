3fb602e1d1ee75b6ce81d4e4bf8650c15a2e7966
Pval <lujanschiiandrei@gmail.com>
2026-08-30 02:24:39 +0100

    HeWasBoilingPureAngerInAKettle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,24 +1,27 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody rb;
-
-    
     
     [Header("Movement")]
     [SerializeField] Transform orientation;
     Vector2 movementInput;
     Vector3 movementDirection;
+    Vector2 dodgeInput = new Vector2(0f,1f);
 
     [Header("Movement Variable")]
     [SerializeField] float moveSpeed;
     [SerializeField] float dodgeSpeed; 
+    [SerializeField] float dodgeLength;
+    bool isDodging = false;
 
     [Header("Cooldowns")]
-    [SerializeField] float dodgeCooldown;
+    [SerializeField] float dodgeCooldownLength;
+    float dodgeCooldown;
 
     void Start()
     {
@@ -27,9 +30,21 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        movementDirection = orientation.forward * movementInput.y + orientation.right * movementInput.x;
-        rb.AddForce(movementDirection * moveSpeed, ForceMode.Force);
         dodgeCooldown -= Time.deltaTime;
+
+        if (movementInput != Vector2.zero)
+        {
+            dodgeInput = movementInput;
+        }
+
+        if (!isDodging)
+        {
+            rb.linearVelocity = new Vector3(movementInput.x * moveSpeed, rb.linearVelocity.y, movementInput.y * moveSpeed);
+        }
+        else 
+        {
+            rb.linearVelocity = new Vector3(dodgeInput.x * dodgeSpeed, rb.linearVelocity.y, dodgeInput.y * dodgeSpeed);
+        }
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -41,8 +56,15 @@ public class PlayerController : MonoBehaviour
     {
         if (context.performed && dodgeCooldown <= 0f)
         {
-            rb.AddForce(movementDirection * dodgeSpeed, ForceMode.Impulse);
-            dodgeCooldown = 1.5f;
+            StartCoroutine("DodgeDuration");
+            dodgeCooldown = dodgeCooldownLength;
         }
+    }
+
+    private IEnumerator DodgeDuration()
+    {
+        isDodging = true;
+        yield return new WaitForSeconds(dodgeLength);
+        isDodging = false;
     }
 }
